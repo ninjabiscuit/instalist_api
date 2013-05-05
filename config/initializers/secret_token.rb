8145c38c2b828876588fe6546bf531e888ddf91d
Andrew Walker <andy@madebymany.co.uk>
2013-05-05 11:08:56 +0100

    new rails 4 project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+InstalistApi::Application.config.secret_key_base = 'cdee58ce6fcb162922657b7992f9453db8e3e135367230c2ab0685aba9f27d9e283b5303c9c4be30f0a4dd2fc56247bf70185c5dead523fc485e9965698cc419'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+InstalistApi::Application.config.session_store :cookie_store, key: '_instalist_api_session'
