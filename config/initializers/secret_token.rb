46306328d7eda0cb70f88297c9b44d44c5aacc51
Andrew Walker <andy@madebymany.co.uk>
2013-05-05 09:29:48 +0100

    trashed old version

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-InstalistApi::Application.config.secret_token = '8c7a2452932b494e6e1c294bb57948ac1353031d04401f53590c097473ecabdd720e48056f28aca01ad266c1ec238406cf0048e249222f0b8e4cceb175c15ead'
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-InstalistApi::Application.routes.draw do
-  devise_for :users
-
-  root :to => "home#index"
-end
